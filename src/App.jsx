c8c1d3c1c7a3b212ad41884db49c7b5e7a79bf59
Ahmed Mansour <ahmed.om@gmail.com>
2023-07-19 14:53:56 -0400

    Boilerplate: Tailwind + Vite + React
    This commit marks and almost empty project with all the boilerplate
    needed React and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-orange-600 h-screen w-screen'>
+    <h1 className="text-violet-900 text-9xl">MECHANIZM.GAMES</h1>
+    </div>
+  )
+}
+
+export default App
